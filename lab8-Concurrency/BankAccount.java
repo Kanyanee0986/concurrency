7868bf8eba9927a4e6b2b3be80a6c3fdb834a8b5
Kanyanee0986 <kanyanee.san@ku.th>
2025-08-21 17:40:04 +0700

    Add files via upload

---
+++ lab8-Concurrency/BankAccount.java
@@ -0,0 +1,16 @@
+public class BankAccount {
+
+    private long balance ;
+
+    public synchronized void deposit(long amount){
+        balance += amount;
+    }
+     public synchronized void withdraw(long amount){
+        if(balance >= amount)
+             balance -= amount;
+    }
+     public long getBalance(){
+        return this.balance;
+    }
+    
+}
